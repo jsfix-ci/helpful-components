06607b245fff80875235f104950d1e655daac311
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2018-10-14 17:45:42 -0400

    updated overlay component

--- src/overlay/index.js
+++ src/overlay/index.js
@@ -1,48 +1,41 @@
 import React from "react"
 import PropTypes from "prop-types"
-import {
-    Container
-} from "./elements"
+import { Container } from "./elements"
 
 class Overlay extends React.Component {
-    static propTypes = {
-        children: PropTypes.oneOfType([
-            PropTypes.arrayOf(PropTypes.node),
-            PropTypes.node
-        ]).isRequired,
-        content: PropTypes.node.isRequired
-    }
-    state = {
-        isVisible: false
-    }
-    handleMouseOver = () => {
-        this.setState({
-            isVisible: true
-        })
-    };
-    handleMouseLeave = () => {
-        this.setState({
-            isVisible: false
-        })
-    }
-    render() {
-        const {
-            isVisible
-        } = this.state
-        const {
-            children,
-            content
-        } = this.props
-        const renderedContent = (isVisible ? content : null)
-        return (
-            <Container
-                onMouseOver={this.handleMouseOver}
-                onMouseLeave={this.handleMouseLeave}>
-                {renderedContent}
-                {children}
-            </Container>
-        )
-    }
+	static propTypes = {
+		children: PropTypes.oneOfType([
+			PropTypes.arrayOf(PropTypes.node),
+			PropTypes.node
+		]).isRequired,
+		render: PropTypes.func.isRequired
+	}
+	state = {
+		isVisible: false
+	}
+	handleMouseOver = () => {
+		this.setState({
+			isVisible: true
+		})
+	}
+	handleMouseLeave = () => {
+		this.setState({
+			isVisible: false
+		})
+	}
+	render() {
+		const { isVisible } = this.state
+		const { children, render } = this.props
+		const rendered = isVisible ? render() : null
+		return (
+			<Container
+				onMouseOver={this.handleMouseOver}
+				onMouseLeave={this.handleMouseLeave}>
+				{rendered}
+				{children}
+			</Container>
+		)
+	}
 }
 
 export default Overlay
