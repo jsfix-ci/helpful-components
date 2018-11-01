cbb6b8b453491e99145932ed643bbd6964ccc681
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2018-10-31 21:08:44 -0400

    added classnames to form field error component container

--- src/form-field-error/index.js
+++ src/form-field-error/index.js
@@ -1,82 +1,84 @@
 import React from "react"
 import PropTypes from "prop-types"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faExclamationCircle} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons"
 import {
-    Container,
-    TextContainer,
-    Text,
-    CaretContainer,
-    Caret,
+	Container,
+	TextContainer,
+	Text,
+	CaretContainer,
+	Caret
 } from "./elements"
 
 class FormFieldError extends React.Component {
-    static propTypes = {
-        isVisible: PropTypes.bool.isRequired,
-        message: PropTypes.string.isRequired,
-        position: PropTypes.string.isRequired,
-        textStyle: PropTypes.object,
-        bodyStyle: PropTypes.object,
-        onClick: PropTypes.func
-    };
-    static defaultProps = {
-        bodyStyle: {
-            zIndex: 100,
-        },
-        textStyle: {
-            backgroundColor: "red",
-            color: "white",
-            padding: "0.333em",
-            borderRadius: "0.333em",
-        },
-        onClick: () => {}
-    };
-    render() {
-        const {
-            message,
-            position,
-            textStyle,
-            bodyStyle,
-            onClick,
-            isVisible,
-        } = this.props
-        var caretStyle = null
-        if(position == "left"){
-            caretStyle = {
-                borderLeftColor: textStyle.backgroundColor
-            }
-        }
-        if(position == "right"){
-            caretStyle = {
-                borderRightColor: textStyle.backgroundColor
-            }
-        }
-        if(position == "top"){
-            caretStyle = {
-                borderTopColor: textStyle.backgroundColor
-            }
-        }
-        if(position == "bottom") {
-            caretStyle = {
-                borderBottomColor: textStyle.backgroundColor
-            }
-        }
-        return isVisible ? (
-            <Container position={position}
-                       onClick={onClick}
-                       style={bodyStyle}>
-                <CaretContainer>
-                    <Caret position={position}
-                           style={caretStyle}></Caret>
-                </CaretContainer>
-                <TextContainer>
-                    <Text style={textStyle}>
-                        {message}
-                    </Text>
-                </TextContainer>
-            </Container>
-        ) : null
-    }
+	static propTypes = {
+		isVisible: PropTypes.bool.isRequired,
+		message: PropTypes.string.isRequired,
+		position: PropTypes.string.isRequired,
+		textStyle: PropTypes.object,
+		bodyStyle: PropTypes.object,
+		onClick: PropTypes.func,
+		containerClassName: PropTypes.string
+	}
+	static defaultProps = {
+		containerClassName: "form-field-error",
+		bodyStyle: {
+			zIndex: 100
+		},
+		textStyle: {
+			backgroundColor: "red",
+			color: "white",
+			padding: "0.333em",
+			borderRadius: "0.333em"
+		},
+		onClick: () => {}
+	}
+	render() {
+		const {
+			message,
+			position,
+			textStyle,
+			bodyStyle,
+			onClick,
+			isVisible,
+			containerClassName
+		} = this.props
+		var caretStyle = null
+		if (position == "left") {
+			caretStyle = {
+				borderLeftColor: textStyle.backgroundColor
+			}
+		}
+		if (position == "right") {
+			caretStyle = {
+				borderRightColor: textStyle.backgroundColor
+			}
+		}
+		if (position == "top") {
+			caretStyle = {
+				borderTopColor: textStyle.backgroundColor
+			}
+		}
+		if (position == "bottom") {
+			caretStyle = {
+				borderBottomColor: textStyle.backgroundColor
+			}
+		}
+		return isVisible ? (
+			<Container
+				className={containerClassName}
+				position={position}
+				onClick={onClick}
+				style={bodyStyle}>
+				<CaretContainer>
+					<Caret position={position} style={caretStyle} />
+				</CaretContainer>
+				<TextContainer>
+					<Text style={textStyle}>{message}</Text>
+				</TextContainer>
+			</Container>
+		) : null
+	}
 }
 
 export default FormFieldError
